52b1721c135b87a337d612857672f8a43ca48415
Anna Debenham <anna@maban.co.uk>
2011-10-04 13:47:05 +0100

    Removing code examples from templates page to make getting started easier

--- static-files/templates.php
+++ static-files/templates.php
@@ -8,58 +8,29 @@
 
 <h1>Templates</h1>
 
-<article class="snippet" id="snippet-template-1">
-	<h1>Very basic template</h1>
-	<div class="examples">
-		<div class="how-it-works">
-			<h2>The code</h2>
-			<pre class="code"><code class="prettyprint lang-html"><?php include_once('library/snippet/template-1.html') ?></code></pre>
-		</div>
-		<div class="how-it-looks">
-			<h2>How it looks</h2>
-			<div class="preview">
-				<img src="library/asset/template-1.png" alt="Screenshot of template 1" />
-				<!--<a href="library/demo/template-1.html">View example</a>-->
-				<a class="button" href="http://jsbin.com/osajac/36/edit#html,live">Hack this</a>
-			</div>
-		</div>
-	</div>
+
+<article class="template">
+	<a href="http://jsbin.com/osajac/36/edit#html,live">
+		<h1>Simple 1 Column</h1>
+		<img src="library/asset/template-1.png" alt="Screenshot of template 1" />
+		<p class="button">Hack this</p>
+	</a>
 </article>
 
-<article class="snippet" id="snippet-template-2">
-	<h1>2 columns</h1>
-	<div class="examples">
-		<div class="how-it-works">
-			<h2>The code</h2>
-			<pre class="code"><code class="prettyprint lang-html"><?php include_once('library/snippet/template-2.html') ?></code></pre>
-		</div>
-		<div class="how-it-looks">
-			<h2>How it looks</h2>
-			<div class="preview">
-				<img src="library/asset/template-2.png" alt="Screenshot of template 2" />
-				<!--<a href="library/demo/template-2.html">View example</a>-->
-				<a class="button" href="http://jsbin.com/ehebip/2/edit#html,live">Hack This</a>
-			</div>
-		</div>
-	</div>
+<article class="template">
+	<a href="http://jsbin.com/ehebip/2/edit#html,live">
+		<h1>Powder 2 Column</h1>
+		<img src="library/asset/template-2.png" alt="Screenshot of template 2" />
+		<p class="button">Hack This</p>
+	</a>
 </article>
 
-<article class="snippet" id="snippet-template-3">
-	<h1>3 columns</h1>
-	<div class="examples">
-		<div class="how-it-works">
-			<h2>The code</h2>
-			<pre class="code"><code class="prettyprint lang-html"><?php include_once('library/snippet/template-3.html') ?></code></pre>
-		</div>
-		<div class="how-it-looks">
-			<h2>How it looks</h2>
-			<div class="preview">
-				<img src="library/asset/template-3.png" alt="Screenshot of template 3" />
-				<!--<a href="library/demo/template-2.html">View example</a>-->
-				<a class="button" href="http://jsbin.com/eqetow/2/edit#html,live">Hack This</a>
-			</div>
-		</div>
-	</div>
+<article class="template">
+	<a href="http://jsbin.com/eqetow/2/edit#html,live">
+		<h1>Beige 3 Column</h1>
+		<img src="library/asset/template-3.png" alt="Screenshot of template 3" />
+		<p class="button">Hack This</p>
+	</a>
 </article>
 
 <article class="snippet" id="resources">
